6589ec6c1fbd78fab1638a8c9fb9a25bc44a7ed4
Henrincode <spellctro@gmail.com>
2025-07-06 14:03:31 -0300

    Criado funções para estilizar lista e carregar arquivos de dados

--- script.js
+++ script.js
@@ -1,3 +1,36 @@
+const tarefasBanco = []
+
+carregarDados()
+
+// ESTILOS
+
+const checkboxes = document.querySelectorAll('.tarefa input[type="checkbox"]');
+async function carregarDados() {
+    try {
+        const carregarTarefas = await fetch('./data.json')
+        const dadosTarefas = await carregarTarefas.json()
+
+        tarefasBanco.push(...dadosTarefas)
+
+        console.log(tarefasBanco)
+    } catch (erro) {
+        console.error('Erro ao carregar dados: ', erro)
+    }
+}
+
+// FUNÇÕES
+
 function addTarefa() { }
 function altTarefa() { }
 function delTarefa() { }
+
+checkboxes.forEach(checkbox => {
+    checkbox.addEventListener('change', function () {
+        const tarefa = this.closest('.tarefa');
+        if (this.checked) {
+            tarefa.classList.add('concluida');
+        } else {
+            tarefa.classList.remove('concluida');
+        }
+    });
+});
