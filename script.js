766d96f84c251e23ecf2ab923fecd5b6922670ff
Henrincode <spellctro@gmail.com>
2025-07-08 15:09:12 -0300

    Criado evento de lixeira para apagar tarefas do banco de dados

--- script.js
+++ script.js
@@ -38,6 +38,7 @@ function listarTarefas() {
                 <div class="tarefa" draggable="true" idTarefa="${tarefa.id}">
                     <input type="checkbox">
                     <p>${tarefa.texto}</p>
+                    <i class="lixeira bi bi-trash3"></i>
                 </div>
             `
     })
@@ -50,11 +51,13 @@ function listarTarefas() {
                 <div class="tarefa concluida" idTarefa="${tarefa.id}">
                     <input type="checkbox" checked>
                     <p>${tarefa.texto}</p>
+                    <i class="lixeira bi bi-trash3"></i>
                 </div>
             `
     })
 
     acaoCheckbox()
+    acaoLixeira()
 }
 
 // Se Checkbox for clicado alterna tarefa concluida ou não concluida
@@ -67,9 +70,9 @@ function acaoCheckbox() {
             const idTarefa = tarefa => tarefa.id === Number(pai.getAttribute('idTarefa'))
 
             // alterna estado concluido e faz efeito de sumir
+            tarefasBanco.find(idTarefa).concluido = this.checked
             pai.classList.add('sumindo')
             setTimeout(() => {
-                tarefasBanco.find(idTarefa).concluido = this.checked
                 listarTarefas()
             }, 300)
 
@@ -77,6 +80,24 @@ function acaoCheckbox() {
         });
     });
 
+}
+
+function acaoLixeira() {
+    const lixeiras = document.querySelectorAll('.tarefa .lixeira')
+
+    lixeiras.forEach(lixeira => {
+        lixeira.addEventListener('click', function() {
+            const pai = this.closest('.tarefa')
+            const retornaIndice = tarefa => tarefa.id === Number(pai.getAttribute('idTarefa'))
+            const indice = tarefasBanco.findIndex(retornaIndice)
+
+            tarefasBanco.splice(indice, 1)
+            pai.classList.add('sumindo')
+            setTimeout(() => {
+                listarTarefas()
+            }, 300)
+        })
+    })
 }
 
 // function addTarefa() { }
